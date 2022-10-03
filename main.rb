97595a8ee0d12c8e8dc782d6b53d51d808248846
Shawn M Moore <shawn@render.com>
2022-10-03 12:57:28 -0400

    update

--- main.rb
+++ main.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  ["<tt>REDIS_HOST</tt>: <tt>#{ENV['REDIS_HOST']}</tt>", "<tt>REDIS_PORT</tt>: <tt>#{ENV['REDIS_PORT']}</tt>"].join("\n<br>\n")
+  ["env:", "<tt>REDIS_HOST</tt>: <tt>#{ENV['REDIS_HOST']}</tt>", "<tt>REDIS_PORT</tt>: <tt>#{ENV['REDIS_PORT']}</tt>"].join("\n<br>\n")
 end
